5e79df84d95d46c31e9436bab7a3ce1ee9ad7de0
Jerry Christiansen <jerri@redweb.dk>
2014-05-31 18:09:15 +0200

    [imp] Move Highlights to layout

--- plugins/system/highlight/highlight.php
+++ plugins/system/highlight/highlight.php
@@ -31,57 +31,41 @@ class PlgSystemHighlight extends JPlugin
 	 */
 	public function onAfterDispatch()
 	{
-		// Check that we are in the site application.
-		if (JFactory::getApplication()->isAdmin())
-		{
-			return true;
-		}
+        // Check that we are in the site application.
+        if (JFactory::getApplication()->isAdmin())
+        {
+            return true;
+        }
 
-		// Set the variables.
-		$input = JFactory::getApplication()->input;
-		$extension = $input->get('option', '', 'cmd');
+        // Set the variables.
+            $input = JFactory::getApplication()->input;
+            $extension = $input->get('option', '', 'cmd');
 
-		// Check if the highlighter is enabled.
-		if (!JComponentHelper::getParams($extension)->get('highlight_terms', 1))
-		{
-			return true;
-		}
+        // Check if the highlighter is enabled.
+            if (!JComponentHelper::getParams($extension)->get('highlight_terms', 1))
+            {
+                return true;
+            }
 
-		// Check if the highlighter should be activated in this environment.
-		if (JFactory::getDocument()->getType() !== 'html' || $input->get('tmpl', '', 'cmd') === 'component')
-		{
-			return true;
-		}
+        // Check if the highlighter should be activated in this environment.
+            if (JFactory::getDocument()->getType() !== 'html' || $input->get('tmpl', '', 'cmd') === 'component')
+            {
+                return true;
+            }
 
-		// Get the terms to highlight from the request.
-		$terms = $input->request->get('highlight', null, 'base64');
-		$terms = $terms ? json_decode(base64_decode($terms)) : null;
+        // Get the terms to highlight from the request.
+            $terms = $input->request->get('highlight', null, 'base64');
+            $terms = $terms ? json_decode(base64_decode($terms)) : null;
 
-		// Check the terms.
-		if (empty($terms))
-		{
-			return true;
-		}
+        // Check the terms.
+            if (empty($terms))
+            {
+                return true;
+            }
 
-		// Clean the terms array.
-		$filter = JFilterInput::getInstance();
 
-		$cleanTerms = array();
+        JLayoutHelper::render('plugins.system.highlight.default', array("terms" => $terms));
 
-		foreach ($terms as $term)
-		{
-			$cleanTerms[] = htmlspecialchars($filter->clean($term, 'string'));
-		}
-
-		// Activate the highlighter.
-		JHtml::_('behavior.highlighter', $cleanTerms);
-
-		// Adjust the component buffer.
-		$doc = JFactory::getDocument();
-		$buf = $doc->getBuffer('component');
-		$buf = '<br id="highlighter-start" />' . $buf . '<br id="highlighter-end" />';
-		$doc->setBuffer($buf, 'component');
-
-		return true;
+        return true;
 	}
 }
