ef4fc3988a8f0afc25a097b674325f0e1ed5f43e
Roberto - phproberto <roberto@phproberto.com>
2014-06-01 14:00:55 +0200

    [fix] textarea field not working

--- layouts/libraries/joomla/form/fields/textarea.php
+++ layouts/libraries/joomla/form/fields/textarea.php
@@ -9,38 +9,13 @@
 
 defined('_JEXEC') or die;
 
+extract($displayData);
 
-//		
-$name = 		$displayData[0];
-$id = 			$displayData[1];
-$value = 		$displayData[2];
-$columns = 		$displayData[3];
-$rows = 		$displayData[4];
-$class = 		$displayData[5];
-$readonly =		$displayData[6];
-$disabled = 	$displayData[7];
-$required = 	$displayData[8];
-$hint = 		$displayData[9];
-$autocomplete = $displayData[10];
-$autofocus = 	$displayData[11];
-$spellcheck = 	$displayData[12];
-$onchange = 	$displayData[13];
-$onclick = 		$displayData[14];
-
-$displayData = array($this->name, $this->id, $this->value, $columns, $rows, $class, $readonly, $disabled, $required, $hint, $autocomplete, $autofocus, $spellcheck, $onchange, $onclick);
-
-
-
-		JHtml::_('jquery.framework');
-		JHtml::_('script', 'system/html5fallback.js', false, true);
-
-		echo '<textarea name="' . $this->name . '" id="' . $this->id . '"' . $columns . $rows . $class
-			. $hint . $disabled . $readonly . $onchange . $onclick . $required . $autocomplete . $autofocus . $spellcheck . ' >'
-			. htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '</textarea>';
-
-
-	
-
+JHtml::_('jquery.framework');
+JHtml::_('script', 'system/html5fallback.js', false, true);
 ?>
 
-
+<textarea name="<?php echo $name; ?>" id="<?php echo $id; ?>" <?php echo $columns . $rows . $class
+	. $hint . $disabled . $readonly . $onchange . $onclick . $required . $autocomplete . $autofocus . $spellcheck; ?>
+	><?php echo htmlspecialchars($value, ENT_COMPAT, 'UTF-8'); ?>
+</textarea>
