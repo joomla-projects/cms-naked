0eeba01b8c628598179671d6cc54591a78079e02
Cory Webb <cory.webb@searshc.com>
2014-08-08 15:44:14 -0500

    Fixed typo in form.php and set subpackage in form.php and default.php for content vote plugin

--- plugins/content/vote/tmpl/form.php
+++ plugins/content/vote/tmpl/form.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Plugin
- * @subpackage  Content.pagenavigation
+ * @subpackage  Content.vote
  *
  * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -13,7 +13,7 @@ defined('_JEXEC') or die;
     <span class="content_vote">
         <label class="unseen element-invisible" for="content_vote_<?php echo $row->id; ?>"><?php echo JText::_('PLG_VOTE_LABEL'); ?></label>
         <?php echo JHtml::_('select.genericlist', $options, 'user_rating', null, 'value', 'text', '5', 'content_vote_' . $row->id); ?>
-        &#160;<input class="btn btn-mini" type="submit" name="submit_vote" value="' . JText::_('PLG_VOTE_RATE') . '" />
+        &#160;<input class="btn btn-mini" type="submit" name="submit_vote" value="<?php echo JText::_('PLG_VOTE_RATE'); ?>" />
         <input type="hidden" name="task" value="article.vote" />
 		<input type="hidden" name="hitcount" value="0" />
         <input type="hidden" name="url" value="<?php echo htmlspecialchars($uri->toString()); ?>" />
